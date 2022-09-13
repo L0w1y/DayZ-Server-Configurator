e2ab5ba2955c3bba2d5bfcaf3bde0cc11001cc5f
Lowly <about.lowly@gmail.com>
2022-09-13 17:48:56 +0700

    Minor UI changes

--- data/ui/make_ui.py
+++
@@ -1,6 +0,0 @@
-import os
-
-path = "C:/Users/E82Y/PycharmProjects/DayzServerLoader/data/ui"
-for file in os.listdir(path):
-    if file.endswith(".ui"):
-        os.system(f"python -m Pyqt5.puic5 {os.path.join(path, file)} -o {file}.py")
