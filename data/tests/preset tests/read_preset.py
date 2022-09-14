6441e0c8aef8fd5aaa65249f5fe2daf14fa28e2c
Lowly <about.lowly@gmail.com>
2022-09-14 13:58:34 +0700

    tests for read_preset.py

--- data/tests/preset tests/read_preset.py
+++ data/tests/preset tests/read_preset.py
@@ -1,11 +1,21 @@
 import os
-import easygui
-from os import listdir
-from os.path import isfile, join
-# presetpath = easygui.diropenbox(default="C:\\Users\E82Y\PycharmProjects\DayZ-Server-Configurator\data\\tests\preset tests\presets")
-presetpath = "C:\\Users\E82Y\PycharmProjects\DayZ-Server-Configurator\data\\tests\preset tests\presets"
-for i in os.listdir(presetpath):
-    with open(f"{presetpath}\\{i}", "r") as reader:
-        line = reader.readlines()
-        for i in line:
-            print(line[line.index(i)])
+# folders
+server_location = str
+profiles_location = str
+mods_location = str
+presets_location = str
+firstStart = bool
+def OnFirstStart(server_path):
+    #checks mods folder
+    if os.path.exists(f"{server_path}\\mods") and os.path.exists(f"{server_path}\\presets") and os.path.exists(f"{server_path}\\profiles"):
+        return True
+    else:
+        if not(os.path.exists(f"{server_path}\\mods")):
+            os.mkdir(f"{server_path}\\mods")
+            mods_location = f"{server_path}\\mods"
+        if not(os.path.exists(f"{server_path}\\presets")):
+            os.mkdir(f"{server_path}\\presets")
+            presets_location = f"{server_path}\\presets"
+        if not(os.path.exists(f"{server_path}\\profiles")):
+            os.mkdir(f"{server_path}\\profiles")
+            profiles = f"{server_path}\\profiles"
